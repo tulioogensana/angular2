3bdd82b35014c5a348e88b4ebfed6c13bb7a9cce
Tulio Gensana <Tulio.Gensana@internetparatodos.com.ar>
2021-11-18 18:11:31 -0300

    Clase #2

---
+++ index.js
@@ -0,0 +1,15 @@
+var nombre;
+nombre = "Tulio Gensana";
+var edad;
+edad = 30;
+var PERSONAJE = {
+    nombre: nombre,
+    edad: edad
+};
+console.log(PERSONAJE);
+/* crear una interface */
+var spiderman = {
+    nombre: "Peter Parker",
+    poderes: ["trepar", "saltar", "otros"]
+};
+console.log(spiderman);
---
+++ index.ts
@@ -0,0 +1,21 @@
+let nombre: string;
+nombre = "Tulio Gensana"
+
+let edad: number;
+edad = 30;
+
+
+const PERSONAJE = {
+    nombre: nombre,
+    edad:edad   
+};
+
+console.log(PERSONAJE);
+
+/* crear una interface */
+var spiderman={
+    nombre: "Peter Parker",
+    poderes: ["trepar", "saltar", "otros"]
+}
+
+console.log(spiderman)
